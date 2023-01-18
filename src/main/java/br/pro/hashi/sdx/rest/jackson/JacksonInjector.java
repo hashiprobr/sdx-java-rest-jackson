912331f4e5df005b4ce92c120f2d75a98f2b6c9b
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-01-18 12:33:35 -0300

    Set fallback type

--- src/main/java/br/pro/hashi/sdx/rest/jackson/JacksonInjector.java
+++ src/main/java/br/pro/hashi/sdx/rest/jackson/JacksonInjector.java
@@ -117,6 +117,7 @@ public class JacksonInjector extends Injector {
 	private void inject(Builder<?> builder, ConverterMapper mapper) {
 		builder.withSerializer(JSON_TYPE, new JacksonSerializer(mapper));
 		builder.withDeserializer(JSON_TYPE, new JacksonDeserializer(mapper));
+		builder.withFallbackTextType(JSON_TYPE);
 	}
 
 	private ObjectMapper newMapper() {
