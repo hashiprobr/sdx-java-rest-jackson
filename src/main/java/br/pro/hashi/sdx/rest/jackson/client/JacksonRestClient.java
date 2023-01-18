2a6067894bb9222d6e0da4207feb4786c3c893bf
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-01-18 14:35:58 -0300

    Add convenience build methods

--- src/main/java/br/pro/hashi/sdx/rest/jackson/client/JacksonRestClient.java
+++ src/main/java/br/pro/hashi/sdx/rest/jackson/client/JacksonRestClient.java
@@ -1,6 +1,7 @@
 package br.pro.hashi.sdx.rest.jackson.client;
 
 import br.pro.hashi.sdx.rest.client.RestClient;
+import br.pro.hashi.sdx.rest.client.RestClientBuilder;
 
 /**
  * Convenience class to quickly build a REST client with Jackson support.
@@ -10,10 +11,19 @@ public final class JacksonRestClient {
 	 * Instantiates a REST client using a specified URL prefix.
 	 * 
 	 * @param urlPrefix the URL prefix
-	 * @return the REST client
+	 * @return the client
 	 */
 	public static RestClient to(String urlPrefix) {
-		return new JacksonRestClientBuilder().build(urlPrefix);
+		return builder().build(urlPrefix);
+	}
+
+	/**
+	 * Convenience method for instantiating a REST client builder.
+	 * 
+	 * @return the client builder
+	 */
+	public static RestClientBuilder builder() {
+		return new JacksonRestClientBuilder();
 	}
 
 	private JacksonRestClient() {
--- src/main/java/br/pro/hashi/sdx/rest/jackson/server/JacksonRestServer.java
+++ src/main/java/br/pro/hashi/sdx/rest/jackson/server/JacksonRestServer.java
@@ -1,6 +1,7 @@
 package br.pro.hashi.sdx.rest.jackson.server;
 
 import br.pro.hashi.sdx.rest.server.RestServer;
+import br.pro.hashi.sdx.rest.server.RestServerBuilder;
 
 /**
  * Convenience class to quickly build a REST server with Jackson support.
@@ -10,10 +11,19 @@ public final class JacksonRestServer {
 	 * Instantiates a REST server using the resources of a specified package.
 	 * 
 	 * @param packageName the package name
-	 * @return the REST server
+	 * @return the server
 	 */
 	public static RestServer from(String packageName) {
-		return new JacksonRestServerBuilder().build(packageName);
+		return builder().build(packageName);
+	}
+
+	/**
+	 * Convenience method for instantiating a REST server builder.
+	 * 
+	 * @return the server builder
+	 */
+	public static RestServerBuilder builder() {
+		return new JacksonRestServerBuilder();
 	}
 
 	private JacksonRestServer() {
