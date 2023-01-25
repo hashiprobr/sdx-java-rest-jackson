32264d0f4d73761453be8c520e7c2a8a340bce4a
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-01-25 14:03:22 -0300

    Align with upstream

--- src/test/java/br/pro/hashi/sdx/rest/jackson/client/JacksonRestClientTest.java
+++ src/test/java/br/pro/hashi/sdx/rest/jackson/client/JacksonRestClientTest.java
@@ -4,30 +4,29 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
 import br.pro.hashi.sdx.rest.client.RestClient;
+import br.pro.hashi.sdx.rest.client.RestClientBuilder;
 
 class JacksonRestClientTest {
 	private RestClient client;
-
-	@BeforeEach
-	void setUp() {
-		client = mock(RestClient.class);
-	}
 
 	@Test
 	void builds() {
 		try (MockedConstruction<JacksonRestClientBuilder> construction = mockBuilderConstruction()) {
 			assertSame(client, JacksonRestClient.to("http://a"));
+			RestClientBuilder builder = construction.constructed().get(0);
+			verify(builder).build("http://a");
 		}
 	}
 
 	private MockedConstruction<JacksonRestClientBuilder> mockBuilderConstruction() {
+		client = mock(RestClient.class);
 		return mockConstruction(JacksonRestClientBuilder.class, (mock, context) -> {
 			when(mock.build(any())).thenReturn(client);
 		});
--- src/test/java/br/pro/hashi/sdx/rest/jackson/server/JacksonRestServerTest.java
+++ src/test/java/br/pro/hashi/sdx/rest/jackson/server/JacksonRestServerTest.java
@@ -4,30 +4,29 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
 import br.pro.hashi.sdx.rest.server.RestServer;
+import br.pro.hashi.sdx.rest.server.RestServerBuilder;
 
 class JacksonRestServerTest {
 	private RestServer server;
-
-	@BeforeEach
-	void setUp() {
-		server = mock(RestServer.class);
-	}
 
 	@Test
 	void builds() {
 		try (MockedConstruction<JacksonRestServerBuilder> construction = mockBuilderConstruction()) {
 			assertSame(server, JacksonRestServer.from("package"));
+			RestServerBuilder builder = construction.constructed().get(0);
+			verify(builder).build("package");
 		}
 	}
 
 	private MockedConstruction<JacksonRestServerBuilder> mockBuilderConstruction() {
+		server = mock(RestServer.class);
 		return mockConstruction(JacksonRestServerBuilder.class, (mock, context) -> {
 			when(mock.build(any())).thenReturn(server);
 		});
