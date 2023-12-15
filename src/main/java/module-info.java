32fc3205e90b84bed76f1a16cdd96be9f82ee042
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-12-15 07:25:23 -0300

    Port to IntelliJ IDEA

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,22 @@
+/*
+ * Copyright (c) 2023 Marcelo Hashimoto.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+
 /**
  * Defines a Jackson extension for sdx-rest.
  */
 module br.pro.hashi.sdx.rest.jackson {
-	requires transitive br.pro.hashi.sdx.rest;
-	requires transitive com.fasterxml.jackson.databind;
+    requires transitive br.pro.hashi.sdx.rest;
+    requires transitive com.fasterxml.jackson.databind;
 
-	exports br.pro.hashi.sdx.rest.jackson;
-	exports br.pro.hashi.sdx.rest.jackson.client;
-	exports br.pro.hashi.sdx.rest.jackson.server;
+    exports br.pro.hashi.sdx.rest.jackson;
+    exports br.pro.hashi.sdx.rest.jackson.client;
+    exports br.pro.hashi.sdx.rest.jackson.server;
 }
